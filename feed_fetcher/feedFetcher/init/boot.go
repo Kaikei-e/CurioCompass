aa1e6ad89c554c430313ea415bc517fada94e314
Kaikei-e <e_kaikei@protonmail.com>
2024-03-02 09:23:44 +0900

    feedFetcher: made the initializer of db connection

--- feed_fetcher/feedFetcher/init/boot.go
+++ feed_fetcher/feedFetcher/init/boot.go
@@ -1,12 +1,36 @@
 package init
 
-import "github.com/joho/godotenv"
+import (
+	"database/sql"
+	"github.com/joho/godotenv"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+	"github.com/uptrace/bun/driver/pgdriver"
+	"os"
+)
 
-func InitDBConnection() {
+func InitDBConnection() *bun.DB {
 	// connect to db
 	err := godotenv.Load()
 	if err != nil {
 		panic(err)
 	}
 
+	POSTGRES_USER := os.Getenv("POSTGRES_USER")
+	POSTGRES_PASSWORD := os.Getenv("POSTGRES_PASSWORD")
+	POSTGRES_DB := os.Getenv("POSTGRES_DB")
+	DB_IP := os.Getenv("DB_IP")
+
+	dsn := "postgres://" + POSTGRES_USER + ":" + POSTGRES_PASSWORD + "@" + DB_IP + "/" + POSTGRES_DB + "?sslmode=disable"
+
+	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
+
+	db := bun.NewDB(sqldb, pgdialect.New())
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	return db
 }
